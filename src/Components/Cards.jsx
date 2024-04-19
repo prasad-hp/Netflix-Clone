5dec1707c4170bc2f312d587879905a9ca883407
Prasad Hp <prasadhp@outlook.in>
2024-04-19 23:56:14 +0530

    Updated the home page

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -18,11 +18,11 @@ function Cards(props) {
   }, []);
   console.log(images)
   return (
-    <div>
-      <h1 className="font-bold text-white text-4xl m-5  ">{props.title}</h1>
-      <div className="flex overflow-x-scroll">
+    <div className="px-5">
+      <h1 className="font-bold text-white text-4xl m-5">{props.title}</h1>
+      <div className="flex overflow-x-scroll overflow-y-hidden no-scrollbar" >
         {images.map((image, index) => (
-          <img className="w-24 mx-5" key={index} src={`https://image.tmdb.org/t/p/original${image.poster_path}`} alt={`Poster ${index}`} />
+          <img className="max-h-96 px-2.5 w-100% hover:scale-110 hover:opacity-100 duration-500" key={index} src={`https://image.tmdb.org/t/p/original${image.poster_path}`} alt={`Poster ${index}`} />
         ))}
       </div>
     </div>
