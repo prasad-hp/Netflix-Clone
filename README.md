0da494176f614da90c7613b79ed64dca39922981
Prasad_Hp <prasadhp@outlook.in>
2024-06-21 14:52:20 +0530

    Updated the Readme

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-#Netflix Clone
+# Netflix Clone
 
-##Started building the netflix clone using
-    HTML
-    CSS
-    ReactJS
-    Recoil
-    TailwindCSS
-    FireBase
-    Google Auth
-    Stripe for Payment
+### This project is a clone of the Netflix homepage. It replicates the frontend design and functionality of Netflix's homepage using modern web technologies. Note that this project does not include any backend logic.
+
+## Technologies Used
+- **ReactJS**: For building the user interface.
+- **TailwindCSS**: For styling the application.
+- **JavaScript (ES6)**: For implementing logic and functionality.
+- **HTML**: For the structure of the web pages.
+
+## Objective
+The objective of this project is to practice and demonstrate skills in frontend development. It serves as a hands-on exercise to enhance proficiency in ReactJS, TailwindCSS, and modern JavaScript.
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -23,18 +23,18 @@ function Navbar(){
             <div className={`flex h-24 text-gray-100 font-semibold items-center justify-between px-16 font-sans text-xl w-screen fixed ${show && "bg-black"}`}>
                 <div className="flex items-center justify-around w-2/5">
                     <img className="w-36 h-26 cursor-pointer" src="../../Images/Netflix_Logo_RGB.png"/>
-                    <h2>Home</h2>
-                    <h2>Series</h2>
-                    <h2>Films</h2>
-                    <h2>Recently Added</h2>
-                    <h2>My List</h2>
+                    <h2 className="hover:cursor-pointer">Home</h2>
+                    <h2 className="hover:cursor-pointer">Series</h2>
+                    <h2 className="hover:cursor-pointer">Films</h2>
+                    <h2 className="hover:cursor-pointer">Recently Added</h2>
+                    <h2 className="hover:cursor-pointer">My List</h2>
                 </div>
                 <div className="flex items-center justify-end w-2/5 space-x-6">
                     {searchBar && <textarea />}
                     <button onClick={()=> setSearchBar(!searchBar)}>    
                         <img src="../../svg/search_white_24dp.svg" className="w-10"/>
                     </button>
-                    <h2>CHILDREN</h2>
+                    <h2 className="hover:cursor-pointer">CHILDREN</h2>
                     <img src="../../svg/notifications_white_24dp.svg" className="w-10"/>
                     <button className="flex items-center cursor-pointer">
                         <img className="h-12" src="../../Images/profile-icon.png"/>
